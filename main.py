c53cccdc2b0091b523e0d84ed53f79cee0e0d998
QA-Engg <tempjira96@gmail.com>
2025-08-12 10:18:14 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_ollama import ChatOllama
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_experimental.tools import PythonREPLTool
+
+def main():
+    print("Start...")
+
+    instructions = """
+    You are an agent designed to write and execute python code to answer questions.
+    You have access to a Python REPL, which you can use to execute python code.
+    If you get an error, debug your code and try again.
+    Only  use the output of your code to answer the question.
+    You might know the answer without running any code, but you should still run the code to get the answer.
+    If it does not seem like you can write code to answer the question, just return "I don't know" as the answer 
+    """
+
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+
+    prompt  = base_prompt.partial(instructions=instructions)
+
+    tools = [PythonREPLTool()]
+
+    agent = create_react_agent(
+        llm=ChatOllama(model="qwen3:4b", temperature=0),
+        tools=tools,
+        prompt=prompt
+    )
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, max_iterations=5, handle_parsing_errors=True)
+
+    agent_executor.invoke(input={"input": "generate and save in current working directory 15 QR codes that point to www.udemy.com/course/langchain, you have qrcode package installed already"})
+
+if __name__  == "__main__":
+    main()
